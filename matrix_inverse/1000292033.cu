1c010e41a849cbb84a43baee5d90e6e8cf657306
liwuqi95 <wuqi.li@mail.utoronto.ca>
2019-02-22 20:08:23 -0500

    minor change

--- matrix_inverse/1000292033.cu
+++ matrix_inverse/1000292033.cu
@@ -17,19 +17,16 @@ void h_inverse(float *A, float *B, int nx, int ny) {
 
 // device-side matrix addition
 __global__ void f_inverse(float *A, float *B, int nx, int ny, int noElems) {
-//    __shared__ float sdata[32][33];
+    __shared__ float sdata[32][32];
 
     int ix = threadIdx.x + blockIdx.x * blockDim.x;
     int iy = threadIdx.y + blockIdx.y * blockDim.y;
     int idx = iy * nx + ix;
 
     if (idx < noElems) {
-//        sdata[threadIdx.y][threadIdx.x] = A[idx];
-//        __syncthreads();
-
-//        B[ix * ny + iy] = A[threadIdx.y][threadIdx.x];
-
-        B[ix * ny + iy] = A[idx];
+        sdata[threadIdx.y][threadIdx.x] = A[idx];
+        __syncthreads();
+        B[ix * ny + iy] = A[threadIdx.y][threadIdx.x];
     }
 }
 
