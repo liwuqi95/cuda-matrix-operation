d1454b4f43e4a03a1efbbb9b1cac1a9d547239d3
liwuqi95 <wuqi.li@mail.utoronto.ca>
2019-02-22 23:45:04 -0500

    minor change

--- matrix_inverse/1000292033.cu
+++ matrix_inverse/1000292033.cu
@@ -36,7 +36,7 @@ __global__ void f_inverse(float *A, float *B, int nx, int ny, int noElems) {
 
         __syncthreads();
 
-        int index_out = (xBlock + threadIdx.y * 32) * ny + yBlock + threadIdx.x;
+        int index_out = xBlock * ny + yBlock + threadIdx.x + 32 * threadIdx.y;
 
         for (int i = 0; i < 32 && index_out < noElems; i++) {
             B[index_out + i * ny] = sdata[threadIdx.x][i];
