aa6c2b2c7fa995c9f6910c924531b557455f08f1
liwuqi95 <wuqi.li@mail.utoronto.ca>
2019-02-24 13:28:33 -0500

    add try

--- matrix_inverse/1000292033.cu
+++ matrix_inverse/1000292033.cu
@@ -31,6 +31,7 @@ __global__ void f_inverse(float *A, float *B, int nx, int ny, int noElems) {
     for (int i = 0; i < 32; i++) {
         x = ix;
         y = iy + nx * i;
+        printf("Try x = %d, y = %d \n", x, y);
         if (x < nx && y < ny) {
             sdata[i][threadIdx.x] = A[y * nx + x];
             printf("From x = %d, y = %d \n", x, y);
